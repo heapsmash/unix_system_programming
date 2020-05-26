aed9c0baed9c550e901fffba5c25cf8781f30a23
Michael Walker <mwalk762@mtroyal.ca>
2020-05-25 19:20:22 -0600

    update to handle -a (append) and no optargs.

--- ch_4/exercise_tee/src/my_tee.c
+++ ch_4/exercise_tee/src/my_tee.c
@@ -1,22 +1,64 @@
 #include "tlpi_hdr.h"
+#include <linux/limits.h>
 #include <getopt.h>
+#include <fcntl.h>
+
+#ifndef BUF_SIZE /* Allow "cc -D" to override definition */
+#define BUF_SIZE 1024
+#endif /* BUF_SIZE */
+#define NAME_LEN
 
 int main(int argc, char *argv[])
 {
-    int opt, append_flag;
+    char buf[BUF_SIZE], *file_name;
+    int opt, tee_fd;
+    ssize_t num_read;
 
-    while ((opt = getopt(argc, argv, "a:") != -1))
+    int append_flag = 0;
+    while ((opt = getopt(argc, argv, "a:")) != -1)
     {
         switch (opt)
         {
         case 'a':
             append_flag = 1;
             break;
-        case 'h':
-            usageErr("%s [-ah] file", argv[0]);
-        default:
-            append_flag = 0;
+        case '?':
+            usageErr("%s [-ah] file\n", argv[0]);
         }
     }
-    puts(argv[optind]);
+
+    if (append_flag == 0)
+    {
+        if (argv[1] == NULL)
+            usageErr("%s [-ah] file\n", argv[0]);
+        file_name = strdup(argv[1]);
+    }
+    else
+    {
+        if (argv[2] == NULL)
+            usageErr("%s [-ah] file\n", argv[0]);
+        file_name = strdup(argv[2]);
+    }
+
+    if (file_name == NULL)
+        usageErr("%s [-ah] file\n", argv[0]);
+
+    if (append_flag)
+        tee_fd = open(file_name, O_WRONLY | O_APPEND);
+    else
+        tee_fd = open(file_name, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (tee_fd == -1)
+        errExit("open");
+
+    while ((num_read = read(STDIN_FILENO, buf, BUF_SIZE)) > 0)
+        if (write(tee_fd, buf, num_read) != num_read)
+            fatal("couldn't write whole buffer");
+
+    if (num_read == -1)
+        errExit("read");
+
+    if (close(tee_fd) == -1)
+        errExit("close");
+
+    exit(EXIT_SUCCESS);
 }
