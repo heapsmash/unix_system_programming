07b6246aaa4265970534abaac31833437bb111d8
Michael Walker <mwalk762@mtroyal.ca>
2020-06-01 21:09:33 -0600

    Add build folder, also start exercise 2

--- ch_5/scatter_input/src/t_readv.c
+++ ch_5/scatter_input/src/t_readv.c
@@ -3,6 +3,10 @@
 #include <fcntl.h>
 #include "tlpi_hdr.h"
 
+// Modify the program in Listing 5-3 to use standard file I/O system calls (open() and
+// lseek()) and the off_t data type. Compile the program with the _FILE_OFFSET_BITS
+// macro set to 64, and test it to show that a large file can be successfully created.
+
 int main(int argc, char *argv[])
 {
     int fd;
